9a5313c5cf09536096f1a706684ca7c53cea730f
dbking <s@ss.ss>
2013-03-01 00:11:41 +0100

    main

--- converter.py
+++ converter.py
@@ -1,5 +1,12 @@
 from font import Font, FontLoader
 
+def main():
+	text = getText()
+	font = FontLoader().loadFont('fancyFont/')
+	drawer = TextDrawer()
+	drawer.setFont(font)
+	drawer.draw(text)
+
 def getText():
 	return 'Git'
 
@@ -10,9 +17,6 @@ class TextDrawer:
 	def draw(self, text):
 		pass
 
-text = getText()
-font = FontLoader().loadFont('fancyFont/')
-drawer = TextDrawer()
-drawer.setFont(font)
-drawer.draw(text)
 
+if __name__ == '__main__':
+	main()
